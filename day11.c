7bb33367585d23114cd577f4e5af031ec01dbe0e
guneet153 <guneetk1811@gmail.com>
2026-02-11 21:45:32 +0530

    commit

---
+++ day11.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Input dimensions
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix1[m][n], matrix2[m][n], result[m][n];
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Add matrices
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Display result
+    printf("Resultant matrix after addition:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
